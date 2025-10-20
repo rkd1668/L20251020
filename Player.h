b2ab4d5468780fd8e5cc3ab45213ec413fe2781a
rkd1668 <koreapenguin1668@gmail.com>
2025-10-20 10:57:52 +0900

    overloading
    overloading

--- Player.h
+++ Player.h
@@ -8,5 +8,22 @@ public:
 	virtual ~APlayer();
 
 	virtual void Tick() override; //이건 부모한테 있는건데 재정의 함, 내 자식도 재정의 가능
+
+	//overloading
+	//name mangling
+	void Jump(void)
+	{
+
+	}
+
+	void Jump(int a)
+	{
+
+	}
+	
+	void Jump(float b)
+	{
+
+	}
 };
 
